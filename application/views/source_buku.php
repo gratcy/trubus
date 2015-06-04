330f89bb952e520bd6c2650ddfb2b4a8309412d5
gratcy <palmagratcy@gmail.com>
2015-06-04 17:09:29 +0700

    wew

--- application/views/source_buku.php
+++ application/views/source_buku.php
@@ -42,7 +42,7 @@ AND c.ibcid ='".$_REQUEST['branch']."' AND ( btitle LIKE '%".$_REQUEST['term']."
 	$query = mysql_query($req);
 	while($row = mysql_fetch_array($query))
 	{
-		$results[] = array('label' => $row['btitle'],'bid' => $row['bid'],'bcode' => $row['bcode'],
+		$results[] = array('label' => $row['bcode'] .' | '.$row['btitle'],'bid' => $row['bid'],'bcode' => $row['bcode'],
 		'bisbn' => $row['bisbn'],'bprice' => $row['bprice'],'bdisc' => $row['bdisc'],'bpublisher' => $row['bpublisher'],'pname' => $row['pname'],'stok'=>$row['istock'],'tqty'=>$row['tqty']);
 	}
 	$this -> memcachedlib -> set('__trans_suggeest_2', json_encode($results), 3600,true);
