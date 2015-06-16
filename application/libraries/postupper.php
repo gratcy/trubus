34581b3e0a8dee05b04b53aa1008ca02c016ca9d
gratcy <palmagratcy@gmail.com>
2015-06-16 10:07:54 +0700

    wew

--- application/libraries/postupper.php
+++ application/libraries/postupper.php
@@ -3,7 +3,7 @@ class Postupper {
     function __construct() {
 		if (!preg_match('/'.addcslashes(site_url(),'/').'(login|users|pm|settings)/i', current_url())) {
 			foreach($_POST as $K => $v) :
-			$_POST[$K] = strtoupper($_POST[$K]);
+			if (!is_array($_POST[$K])) $_POST[$K] = strtoupper($_POST[$K]);
 			endforeach;
 		}
     }
